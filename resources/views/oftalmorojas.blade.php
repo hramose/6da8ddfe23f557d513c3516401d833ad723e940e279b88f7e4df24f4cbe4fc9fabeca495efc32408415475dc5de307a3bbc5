0c174072c1127cf2300bc11d8651fb7919a95c92
jsabbath <zahoz.moopa@gmail.com>
2016-02-16 12:50:59 -0500

    mejora de interfaces
    Mejorando la interfaz
    http://localhost:8000/oftalmorojas

--- resources/views/oftalmorojas.blade.php
+++ resources/views/oftalmorojas.blade.php
@@ -5,8 +5,12 @@
 
 @section('content')
 <!--Titulo de la pagina-->
-<h2 class="gray">
-	Oftalmologica rojas
+<h2 class="container">
+	<div class="row">
+		<div class="col s12 m12 12">
+			<h2>Oftalmologica rojas</h2>
+		</div>
+	</div>
 </h2>
  <!--Parallax plaza constitucion-->
 <div class="parallax-container">
@@ -17,27 +21,87 @@
 <!--Mision vision del proyecto-->
 <div id="intro" class="section scrollspy">
     <div class="container">
-
         <div class="row">
             <div  class="col s12 m4 l4">
                 <div class="center promo promo-example">
-                    <i class="mdi-social-group"></i>
-                    <h5 class="promo-caption"> Mision </h5>
+                    <h5 class="promo-caption"> Quienes somos </h5>
                     <p class="light center">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Cum sociis natoque penatibus et magnis dis parturient montes.</p>
                 </div>
             </div>
             <div class="col s12 m4 l4">
                 <div class="center promo promo-example">
-                    <i class="mdi-social-group"></i>
-                    <h5 class="promo-caption"> Vision </h5>
+                    <h5 class="promo-caption"> Mision </h5>
                     <p class="light center">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
                 </div>
             </div>
             <div class="col s12 m4 l4">
                 <div class="center promo promo-example">
-                    <i class="mdi-social-group"></i>
-                    <h5 class="promo-caption">Objetivos</h5>
+                    <h5 class="promo-caption">Vision</h5>
                     <p class="light center">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<h2 class="container">
+	<div class="row">
+		<h3>Que ofrecemos?</h3>
+	</div>
+</h2>
+
+<div id="intro" class="section scrollspy">
+    <div class="container">
+        <div class="row">
+            <div class="col s12 m4 l4">
+                <div class="center promo promo-example">
+					<div class="card">
+		            <div class="card-image">
+		              <img src="images/sample-1.jpg">
+		              <span class="card-title">Card Title</span>
+		            </div>
+		            <div class="card-content">
+		              <p>I am a very simple card. I am good at containing small bits of information.
+		              I am convenient because I require little markup to use effectively.</p>
+		            </div>
+		            <div class="card-action">
+		              <a href="#">This is a link</a>
+		            </div>
+		          </div>
+                </div>
+            </div>
+            <div class="col s12 m4 l4">
+                <div class="center promo promo-example">
+					<div class="card">
+		            <div class="card-image">
+		              <img src="images/sample-1.jpg">
+		              <span class="card-title">Card Title</span>
+		            </div>
+		            <div class="card-content">
+		              <p>I am a very simple card. I am good at containing small bits of information.
+		              I am convenient because I require little markup to use effectively.</p>
+		            </div>
+		            <div class="card-action">
+		              <a href="#">This is a link</a>
+		            </div>
+		          </div>
+                </div>
+            </div>
+            <div class="col s12 m4 l4">
+                <div class="center promo promo-example">
+		          <div class="card">
+		            <div class="card-image">
+		              <img src="images/sample-1.jpg">
+		              <span class="card-title">Card Title</span>
+		            </div>
+		            <div class="card-content">
+		              <p>I am a very simple card. I am good at containing small bits of information.
+		              I am convenient because I require little markup to use effectively.</p>
+		            </div>
+		            <div class="card-action">
+		              <a href="#">This is a link</a>
+		            </div>
+		          </div>
                 </div>
             </div>
         </div>
