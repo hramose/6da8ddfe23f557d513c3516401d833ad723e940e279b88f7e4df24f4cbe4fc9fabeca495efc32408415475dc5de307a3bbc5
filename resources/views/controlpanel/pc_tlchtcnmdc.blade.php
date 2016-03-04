cd9136bee24ec4a114c096a0309cf6725d4adf51
jsabbath <zahoz.moopa@gmail.com>
2016-03-04 11:11:16 -0500

    chat
    - interfaz chat  50%

---
+++ resources/views/controlpanel/pc_tlchtcnmdc.blade.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html >
+  <head>
+    <meta charset="UTF-8">
+    <title>chateando</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/stilochat.css">
+</head>
+<body>	
+<nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header center">
+          <a href="">
+          	<h2 class="navbar-brand vcenter" href="#">Telemedicina oftalmologica</h2> 
+          </a>
+        </div>
+      </div>
+</nav>
+   <div class="wrapper">
+    <div class="container">
+        <div class="left">
+            <div class="top">
+                <input type="text" />
+                <a href="javascript:;" class="search"></a>
+            </div>
+            <ul class="people">
+                <li class="person" data-chat="person4">
+                    <img src="img/img-user-300x300.jpg" alt="" />
+                    <span class="name">doctor ju</span>
+                    <span class="time">2:09 PM</span>
+                    <span class="preview">It’s not that bad...</span>
+                </li>
+                <li class="person" data-chat="person5">
+                    <img src="img/img-user-300x300.jpg" alt="" />
+                    <span class="name">Doctor corazon</span>
+                    <span class="time">2:09 PM</span>
+                    <span class="preview">Wasup for the third time like is 
+you bling bitch</span>
+                </li>
+                <li class="person" data-chat="person6">
+                    <img src="img/img-user-300x300.jpg" alt="" />
+                    <span class="name">andre</span>
+                    <span class="time">2:09 PM</span>
+                    <span class="preview">howdoyoudoaspace</span>
+                </li>
+            </ul>
+        </div>
+        <div class="right">
+            <div class="top"><span>Para: <span class="name">Doc. octopus</span></span></div>
+            <div class="chat" data-chat="person1">
+                <div class="conversation-start">
+                    <span>Hoy, 6:48 AM</span>
+                </div>
+                <div class="bubble you">
+                    Lorem ipsum dolor sit amet, consect quia.
+                </div>
+                <div class="bubble you">
+                    Lorem ipsum dolor sit amet, coaliquid psa aperiam autem magnam quia, nemo laboriosam.
+                </div>
+                <div class="bubble me">
+                    Lorem ipsum dolor sit amet, conseetur icing e
+                </div>
+            </div>
+            <div class="chat" data-chat="person4">
+                <div class="conversation-start">
+                    <span>Yesterday, 4:20 PM</span>
+                </div>
+                <div class="bubble you">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elidae quam aliquam?
+                </div>
+                <div class="bubble you">
+                    Lorem ipsulaborum incidunt commodi , sunt odio animi corporis perferendis? Dicta?
+                </div>
+                <div class="bubble me">
+                    Lorem ipsum dolor sit amet, consectetupellnt asperiores te laboriosam.
+                </div>
+                <div class="bubble me">
+                    Lorem ipsum dolor sit amet, 
+                </div>
+                <div class="bubble you">
+                    Lorem ipsum dolor sit amet, m.
+                </div>
+                <div class="bubble you">
+                    Lorem ipsum dolor sit amet teneturia, nemo laboriosam.
+                </div>
+            </div>
+            <div class="chat" data-chat="person5">
+                <div class="conversation-start">
+                    <span>Today, 6:28 AM</span>
+                </div>
+                <div class="bubble you">
+                    Wasup
+                </div>
+                <div class="bubble you">
+                    Wasup
+                </div>
+                <div class="bubble me">
+                    Wasup for the third time like is <br />you bling bitch
+                </div>
+            </div>
+            <div class="chat" data-chat="person6">
+                <div class="conversation-start">
+                    <span>Monday, 1:27 PM</span>
+                </div>
+                <div class="bubble you">
+                    So, how's your new phone?
+                </div>
+                <div class="bubble you">
+                    You finally have a smartphone :D
+                </div>
+                <div class="bubble me">
+                    Drake?
+                </div>
+                <div class="bubble me">
+                    Why aren't you answering?
+                </div>
+                <div class="bubble you">
+                    howdoyoudoaspace
+                </div>
+            </div>
+            <div class="write">
+                <a href="javascript:;" class="write-link attach"></a>
+                <input type="text" />
+                <a href="javascript:;" class="write-link smiley"></a>
+                <a href="javascript:;" class="write-link send"></a>
+            </div>
+        </div>
+    </div>
+</div>
+<script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script src="js/indexchat.js"></script>
+  </body>
+</html>
